e1c6bb347c0131e7fb937fca1771a8a917a741e1
Hayden Miedema <haydenmiedema@gmail.com>
2017-10-02 01:57:09 -0400

    update'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,58 +1,58 @@
 class PostsController < ApplicationController
 
-	before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
-	def index
-		if params[:category].blank?
-  			@posts = Post.all.order("created_at DESC")
-  		else
-  			@category_id = Category.find_by(name: params[:category]).id
-  			@posts = Post.where(category_id: @category_id).order("created_at DESC")
-  		end
-	end
+  def index
+    if params[:category].blank?
+      @posts = Post.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @posts = Post.where(category_id: @category_id).order("created_at DESC")
+    end
+  end
 
-	def show
-	end
+  def show
+  end
 
-	def new
-		@post = Post.new
-	end
+  def new
+    @post = Post.new
+  end
 
-	def create
-		@post = Post.new(post_params)
+  def create
+    @post = Post.new(post_params)
 
-     	if @post.save
-          	flash[:success] = "The post was created!"
-     		redirect_to @post 
-     	else 
-          	render ‘new’
-     	end 
-	end
+    if @post.save
+      flash[:success] = "The post was created!"
+      redirect_to @post
+    else
+      render ‘new’
+    end
+  end
 
-	def update
-		if @post.update(post_params)
-        	redirect_to @post, notice: “Update successful”
-   		else
-        	render ‘edit’
-     	end
-	end
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: “Update successful”
+    else
+      render ‘edit’
+    end
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def destroy
-		@post.destroy
-    		redirect_to root_path, notice: “Post destroyed”
-	end
+  def destroy
+    @post.destroy
+    redirect_to _path, notice: “Post destroyed”
+  end
 
-private
-	
-	def post_params
-		params.require(:post).permit(:title, :content, :category_id)
-	end
+  private
 
-	def find_post
-		@post = Post.find(params[:id])
-	end
+  def post_params
+    params.require(:post).permit(:title, :content, :category_id)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
 
 end
